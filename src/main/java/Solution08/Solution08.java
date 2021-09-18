f8eb8cfb6c70d4e8fc074afd7c61b24f2ccd8d98
Emanuel <emanuelpadro@icloud.com>
2021-09-18 19:23:34 -0400

    exercise 8

--- src/main/java/Solution08/Solution08.java
+++ src/main/java/Solution08/Solution08.java
@@ -5,8 +5,37 @@ package Solution08;
  *  Copyright 2021 first_name last_name
  */
 
+import java.util.Scanner;
+
 public class Solution08 {
+    static Solver08 InputHandler(){
+        String UserInput;
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.print("How many People? ");
+        UserInput = Scanner.next();
+        int People = Integer.parseInt(UserInput);
+
+        System.out.print("How many Pizzas do you have? ");
+        UserInput = Scanner.next();
+        int Pizzas = Integer.parseInt(UserInput);
+
+        System.out.print("How many slices per pizza? ");
+        UserInput = Scanner.next();
+        int SlicesPerPizza = Integer.parseInt(UserInput);
+
+        return new Solver08(People,Pizzas,SlicesPerPizza);
+
+    }
+
+    public static void Printer(Solver08 Results){
+        System.out.println(Results.People + " people with " + Results.Pizzas + " pizzas ( " + Results.SlicesTotal + " slices)");
+        System.out.println("Each person gets " + Results.SlicesPerPerson + " pieces of pizza.");
+        System.out.println("There are " + Results.SlicesLeftover + " leftover pieces");
+    }
+
     public static void main(String[] args) {
-        System.out.print("Yo whats up");
+        Solver08 Results = InputHandler();
+        Printer(Results);
     }
 }
